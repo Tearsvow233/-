dd2219ba36d55e98cbf8a41be1716a096a5ac8b9
744748428 <744748428@qq.com>
2026-09-08 21:49:47 +0800

    Revert "哈气动画：60 秒未点击后下次点击触发"
    This reverts commit 7534e241833672b3af1f79d3123e63acaf8c6389.

--- pet_drag.py
+++ pet_drag.py
@@ -11,7 +11,6 @@ import traceback
 from PySide6.QtCore import Qt, QTimer
 
 from drag_physics import DragPhysics
-import pet_hiss
 
 
 class DragMixin:
@@ -108,9 +107,7 @@ class DragMixin:
             # 原地没动 = 单击 → 触发反应
             # 但睡眠中发起的点击（含凑满 3 次刚唤醒那一下）不做普通反应
             if not was_sleep_press:
-                # 超过 60s 没点过猫：本次点击改播哈气动画（pet_hiss 门控）
-                if not pet_hiss.try_play(self):
-                    self.start_react()
+                self.start_react()
         self.last_touch_ts = time.time()
         del self._press_pos
 
